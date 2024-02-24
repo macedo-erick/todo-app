2c29435cff8527faeca4fbc7eab81f447dfe15f4
macedo-erick <macedo.eriick@gmail.com>
2024-02-24 04:21:06 -0300

    ref: Visual changes

--- src/app/private/components/card/card.component.ts
+++ src/app/private/components/card/card.component.ts
@@ -61,7 +61,7 @@ export class CardComponent {
       width: '45rem',
       height: '50rem',
       autoFocus: 'dialog',
-      panelClass: 'overlay__bg'
+      panelClass: 'bg__slate__gray'
     });
   }
 
