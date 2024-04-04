82e10be414ad8a8d8e8276f98ae0a85acede681e
macedo-erick <macedo.eriick@gmail.com>
2024-04-04 00:23:27 -0300

    ref: Added function types

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,7 +2,7 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (): boolean => {
   const jwtService = inject(JwtHelperService);
   const router = inject(Router);
 
--- src/app/guards/logged-in.guard.ts
+++ src/app/guards/logged-in.guard.ts
@@ -2,7 +2,7 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
-export const loggedInGuard: CanActivateFn = () => {
+export const loggedInGuard: CanActivateFn = (): boolean => {
   const jwtService = inject(JwtHelperService);
   const router = inject(Router);
 
