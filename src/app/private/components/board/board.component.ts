a278e9e9adf87eec9c028b5add26b2e017b7977c
macedo-erick <macedo.eriick@gmail.com>
2024-04-04 01:29:33 -0300

    ref: Modified app favicon and page title based on board name

--- src/app/private/components/board/board.component.ts
+++ src/app/private/components/board/board.component.ts
@@ -1,8 +1,10 @@
 import {
   Component,
   computed,
+  effect,
   ElementRef,
   model,
+  OnDestroy,
   Signal,
   ViewChild
 } from '@angular/core';
@@ -10,18 +12,29 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { List } from '../../models/list.model';
 import { Board } from '../../models/board.model';
 import { timer } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'todo-board',
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
-export class BoardComponent {
+export class BoardComponent implements OnDestroy {
   board = model.required<Board>();
   loaded: Signal<boolean> = computed(() => !!this.board());
 
   @ViewChild('boardName') boardName!: ElementRef<HTMLHeadingElement>;
   @ViewChild('boardLists') boardLists!: ElementRef<HTMLOListElement>;
+
+  constructor(private titleService: Title) {
+    effect(() => {
+      this.titleService.setTitle(this.board().name);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.titleService.setTitle('Todo');
+  }
 
   addList(): void {
     this.board.update(({ lists, ...board }) => ({
