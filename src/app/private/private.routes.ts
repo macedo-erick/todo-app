319b1739e74a4e89a9f1ac0149988013f8a6cca9
macedo-erick <macedo.eriick@gmail.com>
2025-09-14 14:57:15 -0300

    ref: Migrated routing to standalone API

--- src/app/private/private.routes.ts
+++ src/app/private/private.routes.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { BoardComponent } from './pages/board/board.component';
 
-const routes: Routes = [
+export const privateRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'board/:id', component: BoardComponent },
   {
@@ -12,9 +11,3 @@ const routes: Routes = [
     pathMatch: 'full'
   }
 ];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class PrivateRoutingModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,27 @@
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { PrivateModule } from './app/private/private.module';
 import { JwtModule } from '@auth0/angular-jwt';
 import {
   provideHttpClient,
   withInterceptorsFromDi
 } from '@angular/common/http';
-import { AppRoutingModule } from './app/app-routing.module';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-export function tokenGetter() {
-  return localStorage.getItem('SESSION');
-}
+import { tokenGetter } from './app/util/util';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(routes),
     importProvidersFrom(
       BrowserModule,
-      AppRoutingModule,
       JwtModule.forRoot({
         config: {
           tokenGetter: tokenGetter,
           allowedDomains: ['localhost:8080']
         }
-      }),
-
-      PrivateModule
+      })
     ),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptorsFromDi())
